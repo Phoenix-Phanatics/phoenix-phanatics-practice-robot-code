ecf4a7efb5818cb2051dc691c60fecbfb88a8991
cuber125 <canocortessamuel@gmail.com>
2025-09-18 21:37:55 -0400

    post teeth

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,12 +46,12 @@ public final class Constants {
     public static final double kBackRightChassisAngularOffset = Math.PI / 2;
 
     // SPARK MAX CAN IDs
-    public static final int kFrontLeftDrivingCanId = 1;
+    public static final int kFrontLeftDrivingCanId = 2;
     public static final int kRearLeftDrivingCanId = 4;
     public static final int kFrontRightDrivingCanId = 6;
     public static final int kRearRightDrivingCanId = 8;
 
-    public static final int kFrontLeftTurningCanId = 2;
+    public static final int kFrontLeftTurningCanId = 1;
     public static final int kRearLeftTurningCanId = 3;
     public static final int kFrontRightTurningCanId = 5;
     public static final int kRearRightTurningCanId = 7;
